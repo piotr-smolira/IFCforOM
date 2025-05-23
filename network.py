b24f81f246f8256417d627bf1a28a37bf8cc1fbc
piotr-smolira <pimas@dtu.dk>
2025-05-23 23:24:45 +0200

    removed redundant while and if statements

--- network.py
+++ network.py
@@ -22,13 +22,7 @@ for decl in schema.declarations():
         # Getting inverse attributes
         for attr in decl.all_inverse_attributes():
             ty = attr.entity_reference()
-            while isinstance(ty, W.aggregation_type):
-                ty = ty.type_of_element()
-            while isinstance(ty, W.named_type):
-                ty = ty.declared_type()
-            if isinstance(ty, (W.entity, W.select_type)):
-                # only entity, using types (e.g IfcLabel) makes no sense
-                G.add_edge(decl.name(), ty.name(), name=f"--{attr.name()}-->")
+            G.add_edge(decl.name(), ty.name(), name=f"--{attr.name()}-->")
         if st := decl.supertype():
             G.add_edge(st.name(), decl.name(), name="<--inh--")
             # print(f"{st.name()} <--inh-- {decl.name()}")
@@ -38,13 +32,14 @@ for decl in schema.declarations():
             for d in decl.select_list():
                 G.add_edge(decl.name(), d.name(), name="<--impl--")
 
-
-""""
+"""
 # Explicit start and end nodes
 start_node = "IfcWall"
 end_node = "IfcWallType"
 # Find the shortest path
 pth = nx.shortest_path(G, start_node, end_node)
+args = [G.edges[pth[i], pth[i + 1]]["name"] for i in range(len(pth) - 1)]
+print(*[*itertools.chain.from_iterable(zip(pth, [*args, ""]))])
 """
 
 pth = nx.shortest_path(G, *sys.argv[1:])
